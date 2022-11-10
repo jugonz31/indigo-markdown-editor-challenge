0d9759d1024b4e39ef6363d39c70a79d8110a3ca
Juan Pablo Gonzalez Alvarez <juanpgonzalez99@gmail.com>
2022-11-10 08:38:44 -0500

    Adding my solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { useState } from 'react';
+import styles from './App.module.css';
+
 function App() {
+  const [textInput, setTextInput] = useState('');
+
+  const transformText = (text: string) => {
+    return text
+      .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+      .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+      .replace(/^---$/gm, '<hr />')
+      .replace(/\n\n(.*$)/gim, '<p>$1</p>')
+      .trim();
+  };
+
   return (
-    <div>
-      <textarea onChange={(e) => console.log(e.target.value)} rows={5} />
-    </div>
+    <main>
+      <h1 className={styles.title}>Indigo Markdown Editor Challenge</h1>
+      <h2 className={styles.subtitle}>By: Juan Gonzalez</h2>
+      <hr />
+      <div className={styles.container}>
+        <h3 className={styles.gridTitle}>Input</h3>
+        <h3 className={styles.gridTitle}>Output</h3>
+        <textarea onChange={(e) => setTextInput(e.target.value)} rows={5} />
+        <div
+          className={styles.result}
+          dangerouslySetInnerHTML={{ __html: transformText(textInput) }}
+        />
+      </div>
+    </main>
   );
 }
 
